f4dc98f5f8d7b93df75be4a074b5d33c08c99e17
Daniel Du <duchangyu@gmail.com>
2016-01-26 17:13:52 +0800

    folder structure refactor

---
+++ countmein-simple/client/index.js
@@ -0,0 +1,42 @@
+
+Template.addCustomers.helpers({
+
+
+});
+
+Template.addCustomers.events({
+    'click button': function() {
+        var customerName = document.getElementById('inputUsername').value;
+        var customerEmail = document.getElementById('inputUseremail').value;
+
+        console.log(customerName + ' - ' + customerEmail);
+
+        if(Customers.find({email : customerEmail}).fetch().length > 0){
+            alert('You have been with us already. thank you!')
+
+        }else{
+
+            Customers.insert({
+                name : customerName,
+                email : customerEmail,
+                createAt : new Date()
+            });
+
+        }
+
+
+
+    }
+
+});
+
+
+Template.body.helpers({
+    'customers' : function(){
+
+        return Customers.find({},{sort : {createAt : -1}});
+
+    }
+
+});
+
