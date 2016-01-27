dec06377c0eb5b3f250da0fe7fab935632309179
Daniel Du <duchangyu@gmail.com>
2016-01-27 14:43:03 +0800

    add customer list

---
+++ app/client/customers/customer.js
@@ -0,0 +1,5 @@
+Template.customer.helper({
+    customer: function(){
+
+    }
+});
--- app/client/customers/customers.js
+++ app/client/customers/customers.js
@@ -1,1 +1,9 @@
 Meteor.subscribe('Customers');
+
+Template.customers.helpers({
+    customers : function(){
+
+        return Customers.find({},{sort : {createAt : -1}});
+    }
+
+});
