bce8fc35f37f1433483bb53e40414447ee584179
Daniel Du <duchangyu@gmail.com>
2016-01-27 15:57:04 +0800

    template subscription, not working

--- app/client/customers/customer.js
+++
@@ -1,5 +0,0 @@
-Template.customer.helper({
-    customer: function(){
-
-    }
-});
--- app/client/customers/customers.js
+++ app/client/customers/customers.js
@@ -1,4 +1,12 @@
-Meteor.subscribe('Customers');
+
+
+Template.customers.onCreated(function() {
+
+    var self = this;
+    self.autorun(function(){
+        self.subscribe('Customers');
+    })
+});
 
 Template.customers.helpers({
     customers : function(){
